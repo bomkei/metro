1c0ab93faece961ece6902760710e5cf4217581d
sssvfw <ssscfw3@gmail.com>
2022-07-26 18:11:00 +0900

    parse command line of line counter

--- checker/__main__.py
+++ checker/__main__.py
@@ -7,6 +7,10 @@ from line_counter import *
 COM_CHECK_DUPLICATE     = '-dup.'
 COM_LINE_COUNT          = '-lcount.'
 
+def error(msg):
+  print(msg)
+  os._exit(1)
+
 def trim_with_comma(com, line):
   return line[len(com):].split(',')
 
@@ -20,7 +24,16 @@ try:
     if arg.startswith(COM_CHECK_DUPLICATE):
       com_check_duplicate(trim_with_comma(COM_CHECK_DUPLICATE, arg))
     elif arg.startswith(COM_LINE_COUNT):
-      com_line_count(trim_with_comma(COM_CHECK_DUPLICATE, arg))
+      trimmed = trim_with_comma(COM_LINE_COUNT, arg)
+      extlist = [ ]
+
+      if trimmed[0].startswith('ext='):
+        extlist = trimmed[0][4:].split('.')
+        trimmed = trimmed[1:]
+      else:
+        error('expected extensions list at first of arguments')
+
+      com_line_count(trimmed, extlist)
     else:
       print(f'unknown argument: {arg}')
 
