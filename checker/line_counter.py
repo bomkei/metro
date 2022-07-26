0c60bca62dafe54cdda79078d167aefd73639b5f
bomkei <93323570+bomkei@users.noreply.github.com>
2022-07-26 18:22:57 +0900

    [Checker] [Line Counter] get files recursively & refactor

--- checker/line_counter.py
+++ checker/line_counter.py
@@ -5,14 +5,17 @@ from collections import defaultdict
 from colors import *
 
 def com_line_count(folders: list, ext: list):
+  total = 0
+
   for folder in folders:
-    total = 0
-    path = f"./{folder}/"
-    files = glob.glob(f"{path}*")
+    path = f'./{folder}/'
+    files = glob.glob(f'{path}/**/*')
+    print(f'\n{folder}:')
+
     for file in files:
       if os.path.splitext(file)[1][1:] in ext:
         count = sum([1 for _ in open(file)])
         total += count
-        print(f"{os.path.basename(file)}: {count}")
-    print(f"\ntotal: {total}")
-  return
+        print(f'  {os.path.basename(file)}: {count}')
+
+  print(f'\ntotal: {total}')
